68c34cf72a669977ff4139a3245bc687415648b5
Moisés Rocha <moisesrocha99@gmail.com>
2020-03-05 19:09:19 +0000

    fixed footer

--- pages/query.php
+++ pages/query.php
@@ -1,10 +1,45 @@
 <html>
-    <head>
-    <?php include_once('../templates/bootstrap_includes.php'); ?>
-    </head>
-    <body>
-    <?php include_once('../templates/header.php'); ?>
 
-    <?php include_once('../templates/footer.php');?>
-    </body>
+<head>
+  <?php include_once('../templates/bootstrap_includes.php'); ?>
+</head>
+
+<body>
+  <?php include_once('../templates/header.php'); ?>
+  <div class="container">
+    <div class="row">
+      <div class="col-3 p-1 bg-light">
+        <form>
+          <label for="firearmCat">Firearms</label>
+          <input type="checkbox" name="category" id="firearmCat" value="firearms">
+          <label for="swordsCat">Swords</label>
+          <input type="checkbox" name="category" id="swordsCat" value="swords">
+          <label for="bowsCat">Bows/Crossbows</label>
+          <input type="checkbox" name="category" id="bowsCat" value="bows">
+        </form>
+      </div>
+
+      <div class="col-9 p-1 bg-light">
+        <?php for ($i=0; $i < 10; $i++) { ?>
+          <div class="card shadow-sm rounded-0 border-0 m-1">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="/assets/logo.png" class="card-img" alt="logo">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+              <a href="#" class="stretched-link"></a>
+            </div>
+          </div>
+        <?php } ?>
+      </div>
+    </div>
+  </div>
+  <?php include_once('../templates/footer.php'); ?>
+</body>
+
 </html>
--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<footer id="sticky-footer" class="py-4 fixed-bottom">
+<footer id="sticky-footer" class="py-4">
   <div class="">
     <h6>© 2020 Copyright: <a href="https://www.feup.com"> Bazooki.com </a>
       <span class="footer-links">
