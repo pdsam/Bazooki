68c0be3c0bc2be8fc8cef724ced9124a359260e3
GambuzX <mg_mesquita@hotmail.com>
2020-03-06 12:59:47 +0000

    Landing page carousel

--- pages/landing_page.php
+++ pages/landing_page.php
@@ -1,82 +1,102 @@
 <head>
 	<title>Bazooki</title>
-    <link rel="stylesheet" href="css/landing_page.css">
-    <link rel="stylesheet" href="css/lib/bootstrap.min.css">
-    <script type="text/javascript" src="js/lib/jquery-3.4.1.min.js"></script>
-    <script type="text/javascript" src="js/lib/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../css/landing_page.css">
+    <link rel="stylesheet" href="../css/lib/bootstrap.min.css">
+    <script type="text/javascript" src="../js/lib/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="../js/lib/bootstrap.min.js"></script>
 
-</head>
-
-<body>
-    <div class="container-fluid">
-        <div id="carouselExample" class="carousel slide" data-ride="carousel" data-interval="9000">
-            <div class="carousel-inner row w-10 mxauto" role="listbox">
-                <div class="carousel-item col-md-4 active">
-                    <img class="img-fluid mx-auto d-bloc" src="/assets/logo.png" alt="slide 1">
-                </div>
-                <div class="carousel-item col-md-4">
-                    <img class="img-fluid mx-auto d-block" src="/assets/logo.png" alt="slide 2">
-                </div>
-                <div class="carousel-item col-md-4">
-                    <img class="img-fluid mx-auto d-block" src="/assets/logo.png" alt="slide 3">
-                </div>
-                <div class="carousel-item col-md-4">
-                    <img class="img-fluid mx-auto d-block" src="/assets/logo.png" alt="slide 4">
-                </div>
-                <div class="carousel-item col-md-4">
-                    <img class="img-fluid mx-auto d-block" src="/assets/logo.png" alt="slide 5">
-                </div>
-                <div class="carousel-item col-md-4">
-                    <img class="img-fluid mx-auto d-block" src="/assets/logo.png" alt="slide 6">
-                </div>
-                <div class="carousel-item col-md-4">
-                    <img class="img-fluid mx-auto d-block" src="/assets/logo.png" alt="slide 7">
-                </div>
-                <div class="carousel-item col-md-4">
-                    <img class="img-fluid mx-auto d-block" src="/assets/logo.png" alt="slide 7">
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
-                <i class="fa fa-chevron-left fa-lg text-muted"></i>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next text-faded" href="#carouselExample" role="button" data-slide="next">
-                <i class="fa fa-chevron-right fa-lg text-muted"></i>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-    </div>
-        
-    </div>
     <script type="text/javascript">
-        $('#carouselExample').on('slide.bs.carousel', function (e) {
-
-            /*
-
-            CC 2.0 License Iatek LLC 2018
-            Attribution required
-
-            */
-
-            let $e = $(e.relatedTarget);
-
-            let idx = $e.index();
-
-            let itemsPerSlide = 8;
-            let totalItems = $('.carousel-item').length;
-
-            if (idx >= totalItems-(itemsPerSlide-1)) {
-                let it = itemsPerSlide - (totalItems - idx);
-                for (let i=0; i<it; i++) {
-                    // append slides to end
-                    if (e.direction=="left") {
-                        $('.carousel-item').eq(i).appendTo('.carousel-inner');
-                    }
-                    else {
-                        $('.carousel-item').eq(0).appendTo('.carousel-inner');
-                    }
-                }
-            }
+        $(document).ready(function(){
+            //Event for pushed the video
+            $('#myCarousel').carousel({
+                pause: true,
+                interval: false
+            });
         });
     </script>
+
+</head>
+<body>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="row"> 
+                    <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="../assets/logo.png" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="../assets/logo.png" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="../assets/logo.png" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="../assets/logo.png" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>                
+            </div>
+            <div class="carousel-item">
+                <div class="row"> 
+                    <div class="col-4">
+                        <img class="d-block w-100" src="../assets/logo.png" alt="First slide">
+                    </div>
+                    <div class="col-4">
+                        <img class="d-block w-100" src="../assets/logo.png" alt="First slide">
+                    </div>
+                    <div class="col-4">
+                        <img class="d-block w-100" src="../assets/logo.png" alt="First slide">
+                    </div>
+                </div>  
+            </div>
+            <div class="carousel-item">
+                <div class="row"> 
+                    <div class="col-4">
+                        <img class="d-block w-100" src="../assets/logo.png" alt="First slide">
+                    </div>
+                    <div class="col-4">
+                        <img class="d-block w-100" src="../assets/logo.png" alt="First slide">
+                    </div>
+                    <div class="col-4">
+                        <img class="d-block w-100" src="../assets/logo.png" alt="First slide">
+                    </div>
+                </div>  
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 </body>
