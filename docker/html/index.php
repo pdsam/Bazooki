f88ecd42f41e07653c4e1b35cc19aa878c6778f1
GambuzX <mg_mesquita@hotmail.com>
2020-03-06 11:47:55 +0000

    docker stuff

---
+++ docker/html/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Rekt</title>
+  </head>
+  <body>
+    <script>alert("h4cK3d :))")</script>
+    <img src="pwned.jpg" />
+  </body>
+</html> 
