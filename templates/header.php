d267ddabb3ea2aff2361331db92db68b87379ccf
JosA(c) Nuno Branco Lima Oliveira e Silva <up201705591@gnomo.fe.up.pt>
2020-03-10 18:13:02 +0000

    merda

--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,8 @@
     <title>Bazooki</title>
 	
 	<?php include_once('../templates/bootstrap_includes.php') ?>
+      <?php $path = explode('/', $_SERVER['REQUEST_URI']); array_pop($path); array_pop($path); $files_path = join("/", $path);?>
+	  <link rel="stylesheet" href="<? echo $files_path . '/css/components/header.css';?>">
 
   </head>
   <body>
