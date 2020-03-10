06339a290cb0383adba31e045d8b81cab00bc740
GambuzX <mg_mesquita@hotmail.com>
2020-03-10 23:29:34 +0000

    login page more pipi

--- pages/login.php
+++ pages/login.php
@@ -8,39 +8,51 @@
             <?php include_once("../templates/navbar.php"); ?>
         </div>
 
-        <div id="login-container" class="text-center container">
-            <form class="form-signin">
-                <img class="mb-4" src="../assets/panda_transparency.png" alt="" width="150" height="150">
-                <h1 class="h3 mb-3 font-weight-normal">Welcome back!</h1>
-                
-                <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-                
-                <label for="inputPassword" class="sr-only">Password</label>
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" value="remember-me"> Keep me logged in 
-                    </label>
-                </div>
-                
-                <button class="btn btn-lg btn-primary btn-block" type="submit">SIGN IN</button>
-                             
+        <div class="container-fluid row"> 
+            <div class="col-lg-5 col-sm-12">
+                <div id="login-container" class="text-center">
+                    <form class="form-signin">
+                        <img class="mb-4" src="../assets/panda_transparency.png" alt="" width="150" height="150">
+                        <h1 class="h3 mb-3 font-weight-normal">Welcome back Bazooker!</h1>
+                        
+                        <label for="inputEmail" class="sr-only">Email address</label>
+                        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                        
+                        <label for="inputPassword" class="sr-only">Password</label>
+                        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                        
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" value="remember-me"> Keep me logged in 
+                            </label>
+                        </div>
+                        
+                        <button class="btn btn-lg btn-primary btn-block" type="submit">SIGN IN</button>
+                                    
 
-                <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign in with Google</button>
-   
-                <div id="forgot-password">
-                    <a href="#recover-password">Forgot your password?</a>
+                        <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign in with Google</button>
+        
+                        <div id="forgot-password">
+                            <a href="#recover-password">Forgot your password?</a>
+                        </div>
+                        
+                        <div id="register-prompt">
+                            <p>Want to become a bazooker? <a href="register.php">Register here</a></p>
+                        </div>
+                    </form>
                 </div>
-                
-                <div id="register-prompt">
-                    <p>Want to become a bazooker?</p>
-                    <a href="register.php">Register here</a>
+                <?php include_once("../templates/footer.php"); ?>
+            </div>
+            <div id="login-img" class="col-lg-7 col-sm-12 text-center">
+                <img src="../assets/login_img.jpg">
+                <div class="jumbotron">
+                    <h1 class="display-4"></h1>
+                    <hr class="my-4">
+                    <p class="lead">“The whole point of collecting is the thrill of acquisition, which must be maximized, and maintained at all costs.”</p>
+                    <p>John Baxter, A Pound of Paper: Confessions of a Book Addict</p>
                 </div>
-            </form>
+            </div>
         </div>
             
-        <?php include_once("../templates/footer.php"); ?>
     </body>
 </html>
