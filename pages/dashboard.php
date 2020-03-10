d861d6e03d6a53e8c11082756dfbf8fc988a5cb5
Moisés Rocha <moisesrocha99@gmail.com>
2020-03-10 18:53:19 +0000

    dashboard progress

---
+++ pages/dashboard.php
@@ -0,0 +1,213 @@
+<?php include_once('../templates/bootstrap_includes.php') ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php $path = explode('/', $_SERVER['REQUEST_URI']);
+    array_pop($path);
+    array_pop($path);
+    $files_path = join("/", $path); ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Coisa</title>
+
+    <!-- Bootstrap core CSS -->
+    <?php bootstrap($files_path); ?>
+
+    <!-- Custom styles for this template -->
+    <link href="<?= $files_parh ?>/css/dashboard.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include_once('../templates/header.php') ?>
+
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-12 col-lg-2 bg-light sidebar">
+                <ul class="nav flex-column">
+                    <li class="nav-item py-2">
+                        <a class="nav-link active" href="#">
+                            <i class="fas fa-home"></i>
+                            Dashboard <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item py-2">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-money-bill-wave"></i>
+                            Auctions
+                        </a>
+                    </li>
+                    <li class="nav-item py-2">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-user-check"></i>
+                            Certifications
+                        </a>
+                    </li>
+                    <li class="nav-item py-2">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-users"></i>
+                            Users
+                        </a>
+                    </li>
+                    <li class="nav-item py-2">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-shield-alt"></i>
+                            Moderators
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+            <main class="col-12 col-lg-10 ml-sm-auto px-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2">Dashboard</h1>
+                    <div class="btn-toolbar mb-2 mb-md-0">
+                        <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+                            <span data-feather="calendar"></span>
+                            This week
+                        </button>
+                    </div>
+                </div>
+
+                <h2>Sales</h2>
+
+                <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+
+                <div class="table-responsive">
+                    <table class="table table-striped table-sm">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Header</th>
+                                <th>Header</th>
+                                <th>Header</th>
+                                <th>Header</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1,001</td>
+                                <td>Lorem</td>
+                                <td>ipsum</td>
+                                <td>dolor</td>
+                                <td>sit</td>
+                            </tr>
+                            <tr>
+                                <td>1,002</td>
+                                <td>amet</td>
+                                <td>consectetur</td>
+                                <td>adipiscing</td>
+                                <td>elit</td>
+                            </tr>
+                            <tr>
+                                <td>1,003</td>
+                                <td>Integer</td>
+                                <td>nec</td>
+                                <td>odio</td>
+                                <td>Praesent</td>
+                            </tr>
+                            <tr>
+                                <td>1,003</td>
+                                <td>libero</td>
+                                <td>Sed</td>
+                                <td>cursus</td>
+                                <td>ante</td>
+                            </tr>
+                            <tr>
+                                <td>1,004</td>
+                                <td>dapibus</td>
+                                <td>diam</td>
+                                <td>Sed</td>
+                                <td>nisi</td>
+                            </tr>
+                            <tr>
+                                <td>1,005</td>
+                                <td>Nulla</td>
+                                <td>quis</td>
+                                <td>sem</td>
+                                <td>at</td>
+                            </tr>
+                            <tr>
+                                <td>1,006</td>
+                                <td>nibh</td>
+                                <td>elementum</td>
+                                <td>imperdiet</td>
+                                <td>Duis</td>
+                            </tr>
+                            <tr>
+                                <td>1,007</td>
+                                <td>sagittis</td>
+                                <td>ipsum</td>
+                                <td>Praesent</td>
+                                <td>mauris</td>
+                            </tr>
+                            <tr>
+                                <td>1,008</td>
+                                <td>Fusce</td>
+                                <td>nec</td>
+                                <td>tellus</td>
+                                <td>sed</td>
+                            </tr>
+                            <tr>
+                                <td>1,009</td>
+                                <td>augue</td>
+                                <td>semper</td>
+                                <td>porta</td>
+                                <td>Mauris</td>
+                            </tr>
+                            <tr>
+                                <td>1,010</td>
+                                <td>massa</td>
+                                <td>Vestibulum</td>
+                                <td>lacinia</td>
+                                <td>arcu</td>
+                            </tr>
+                            <tr>
+                                <td>1,011</td>
+                                <td>eget</td>
+                                <td>nulla</td>
+                                <td>Class</td>
+                                <td>aptent</td>
+                            </tr>
+                            <tr>
+                                <td>1,012</td>
+                                <td>taciti</td>
+                                <td>sociosqu</td>
+                                <td>ad</td>
+                                <td>litora</td>
+                            </tr>
+                            <tr>
+                                <td>1,013</td>
+                                <td>torquent</td>
+                                <td>per</td>
+                                <td>conubia</td>
+                                <td>nostra</td>
+                            </tr>
+                            <tr>
+                                <td>1,014</td>
+                                <td>per</td>
+                                <td>inceptos</td>
+                                <td>himenaeos</td>
+                                <td>Curabitur</td>
+                            </tr>
+                            <tr>
+                                <td>1,015</td>
+                                <td>sodales</td>
+                                <td>ligula</td>
+                                <td>in</td>
+                                <td>libero</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </main>
+        </div>
+    </div>
+
+    <?php include_once('../templates/footer.php') ?>
+    <script src="https://kit.fontawesome.com/07ed6a8693.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+    <script src="<?= $files_parh ?>/js/dashboard.js"></script>
+</body>
+
+</html>
