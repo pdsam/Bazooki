d3bcc96db2def2eccc62fa7442cd665206f20137
GambuzX <mg_mesquita@hotmail.com>
2020-03-11 17:24:30 +0000

    Profile tabs

--- pages/profile.php
+++ pages/profile.php
@@ -1,79 +1,141 @@
-<head>
-    <title>Bazooki</title>
-    <?php $path = explode('/', $_SERVER['REQUEST_URI']);
-    array_pop($path);
-    array_pop($path);
-    $files_path = join("/", $path); ?>
-    <?php include_once(getcwd() . '/../templates/header.php') ?>
-    <link rel="stylesheet" href="<?= $files_path ?>/css/profile.css">
-</head>
+<html>
+    <?php include_once('../templates/header.php') ?>
 
-<body>
-    <div class="container-fluid">
-        <?php include_once(getcwd() . '/../templates/navbar.php') ?>
-        <div class="row">
+    <body>
+        <link rel="stylesheet" href="<?= $files_path ?>/css/profile.css">
+        <div class="container">
+            <?php include_once(getcwd() . '/../templates/navbar.php') ?>
+        </div>
+        <div class="container-fluid">
 
-            <div id="profile_pic" class="col-sm-4">
-                <img src="https://picsum.photos/300/300">
-            </div>
-            <div id="profile_description" class="col-sm-8">
-                <div class="jumbotron">
-                    <h1 class="display-4">Mário Gil</h1>
-                    <div id="profile_stats" class="row">
-                        <div class="col-sm-4">
-                            <p>8 Users</p>
+            <div class="row">
+
+                <div id="profile_pic" class="col-sm-4">
+                    <img src="https://picsum.photos/300/300">
+                </div>
+                <div id="profile_description" class="col-sm-8">
+                    <div class="jumbotron">
+                        <h1 class="display-4">Mário Gil</h1>
+                        <div id="profile_stats" class="row">
+                            <div class="col-sm-4">
+                                <p>8 Users</p>
+                            </div>
+                            <div class="col-sm-4">
+                                <p>8 Users</p>
+                            </div>
+                            <div class="col-sm-4">
+                                <p>8 Users</p>
+                            </div>
                         </div>
-                        <div class="col-sm-4">
-                            <p>8 Users</p>
-                        </div>
-                        <div class="col-sm-4">
-                            <p>8 Users</p>
-                        </div>
+
+                        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac luctus diam, non porta augue. Pellentesque tempor vel enim ut posuere. Quisque in sagittis quam, sit amet facilisis diam. Nam tellus ipsum, fringilla non nisl id, molestie volutpat neque. Curabitur quis dapibus justo, eget malesuada mauris. Duis quis finibus magna. Mauris laoreet varius dolor, eget semper odio posuere ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sollicitudin velit sed purus gravida lobortis. Sed ut imperdiet felis. Cras massa mauris, volutpat auctor fringilla nec, imperdiet sit amet mauris. Duis ac lacinia orci. Cras interdum mi dignissim, elementum sapien mattis, pulvinar justo. Duis molestie leo eu nulla suscipit convallis. Nam tristique commodo aliquam.</p>
                     </div>
+                </div>
+                
+            </div>
+        </div>
 
-                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac luctus diam, non porta augue. Pellentesque tempor vel enim ut posuere. Quisque in sagittis quam, sit amet facilisis diam. Nam tellus ipsum, fringilla non nisl id, molestie volutpat neque. Curabitur quis dapibus justo, eget malesuada mauris. Duis quis finibus magna. Mauris laoreet varius dolor, eget semper odio posuere ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sollicitudin velit sed purus gravida lobortis. Sed ut imperdiet felis. Cras massa mauris, volutpat auctor fringilla nec, imperdiet sit amet mauris. Duis ac lacinia orci. Cras interdum mi dignissim, elementum sapien mattis, pulvinar justo. Duis molestie leo eu nulla suscipit convallis. Nam tristique commodo aliquam.</p>
+        <hr class="customhr">
+
+        <ul id="profile_tabs_select" class="nav nav-tabs" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab">Current Bids</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#tabs-2" role="tab">Won Auctions</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#tabs-3" role="tab">My Auctions</a>
+            </li>
+        </ul>
+
+        <!-- Tab panes -->
+        <div id="profile_tabs" class="tab-content">
+            <div class="tab-pane active" id="tabs-1" role="tabpanel">
+                <div class="row">
+                    <?php create_current_bid_card("Pistola super fixe", "Chuck Norris", "999") ?>
+                    <?php create_current_bid_card("Pistola super fixe", "Chuck Norris", "999") ?>
+                    <?php create_current_bid_card("Pistola super fixe", "Chuck Norris", "999") ?>
                 </div>
             </div>
-            
+            <div class="tab-pane" id="tabs-2" role="tabpanel">
+                <div class="row">
+                    <?php create_winning_bid_card("Pistola super fixe", "Chuck Norris", "999") ?>
+                    <?php create_winning_bid_card("Pistola super fixe", "Chuck Norris", "999") ?>
+                    <?php create_winning_bid_card("Pistola super fixe", "Chuck Norris", "999") ?>
+                </div>
+            </div>
+            <div class="tab-pane" id="tabs-3" role="tabpanel">
+                <div class="row">
+                    <?php create_own_bid_card("Pistola super fixe", "Chuck Norris", "999") ?>
+                    <?php create_own_bid_card("Pistola super fixe", "Chuck Norris", "999") ?>
+                    <?php create_own_bid_card("Pistola super fixe", "Chuck Norris", "999") ?>
+                </div>
+            </div>
+        </div>
+        <?php include_once("../templates/footer.php"); ?>
+    </body>
+</html>
+
+<?php function create_current_bid_card($title, $owner, $currentbid) { ?>
+    <div class="col-12 row profile-tab-entry">
+        <div class="col-3 image-tab">
+            <img src="../assets/gun.jpg">
+        </div>
+        <div class="col-7">
+            <div class="jumbotron">
+                <h2><?=$title?></h2>
+                <p class="lead"><?=$owner?></p>
+                <p class="lead">Current bid: <span><?=$currentbid?></span>€</p>
+            </div>
+        </div>
+        <div class="col-2">
+            <a class="btn btn-primary btn-lg centered_vertically">Visit</a>
         </div>
     </div>
+<?php } ?>
 
-    <hr class="customhr">
-
-    <ul id="profile_tabs_select" class="nav nav-tabs" role="tablist">
-        <li class="nav-item">
-            <a class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab">First Panel</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="tab" href="#tabs-2" role="tab">Second Panel</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="tab" href="#tabs-3" role="tab">Third Panel</a>
-        </li>
-    </ul>
-
-    <!-- Tab panes -->
-    <div id="profile_tabs" class="tab-content">
-        <div class="tab-pane active" id="tabs-1" role="tabpanel">
-            <div class="row">
-                <div class="col-12">Row 1</div>
-                <div class="col-12">Row 2</div>
-                <div class="col-12">Row 3</div>
+<?php function create_winning_bid_card($title, $owner, $currentbid) { ?>
+    <div class="col-12 row profile-tab-entry">
+        <div class="col-3 image-tab">
+            <img src="../assets/gun.jpg">
+        </div>
+        <div class="col-6">
+            <div class="jumbotron">
+                <h2><?=$title?></h2>
+                <p class="lead"><?=$owner?></p>
+                <p class="lead">Winning bid: <span><?=$currentbid?></span>€</p>
             </div>
         </div>
-        <div class="tab-pane" id="tabs-2" role="tabpanel">
-            <div class="row">
-                <div class="col-12">Row 4</div>
-                <div class="col-12">Row 5</div>
-                <div class="col-12">Row 6</div>
-            </div>
-        </div>
-        <div class="tab-pane" id="tabs-3" role="tabpanel">
-            <div class="row">
-                <div class="col-12">Row 7</div>
-                <div class="col-12">Row 8</div>
-                <div class="col-12">Row 9</div>
+        <div class="col-3">
+            <div class="jumbotron">
+                <p class="lead">Rating: </p>
+                <span>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </span>
             </div>
         </div>
     </div>
-</body>
+<?php } ?>
+
+<?php function create_own_bid_card($title, $owner, $currentbid) { ?>
+    <div class="col-12 row profile-tab-entry">
+        <div class="col-3 image-tab">
+            <img src="../assets/gun.jpg">
+        </div>
+        <div class="col-7">
+            <div class="jumbotron">
+                <h2><?=$title?></h2>
+                <p class="lead"><?=$owner?></p>
+                <p class="lead">Highest bid: <span><?=$currentbid?></span>€</p>
+            </div>
+        </div>
+        <div class="col-2">
+            <a class="btn btn-primary btn-lg centered_vertically">Visit</a>
+        </div>
+    </div>
+<?php } ?>
