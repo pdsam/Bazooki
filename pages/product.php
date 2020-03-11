cedc3da415ef64fddbc619a01c55a7b3ab06f526
pdsam <paulodsam@gmail.com>
2020-03-11 00:17:41 +0000

    a little bit of styling

--- pages/product.php
+++ pages/product.php
@@ -15,11 +15,11 @@
 
 <body class="bg-light">
     <div class="container mt-2">
-        <?php include_once(getcwd() . '/../templates/header.php'); ?>
+        <?php include_once(getcwd() . '/../templates/navbar.php') ?>
         <div class="row" id="product-details">
             <div class="col-md-5">
-                <div class="card">
-                    <div class="card-body">
+                <div class="card h-100">
+                    <div class="shadow-lg">
                         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                             <ol class="carousel-indicators">
                                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -29,13 +29,13 @@
 
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
-                                    <img class="d-block w-100" src="http://lorempixel.com/400/400" alt="First slide">
+                                    <img class="d-block w-100 image-responsive" src="http://lorempixel.com/400/400" alt="First slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block w-100" src="http://lorempixel.com/400/400" alt="Second slide">
+                                    <img class="d-block w-100 image-responsive" src="http://lorempixel.com/400/400" alt="Second slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block w-100" src="http://lorempixel.com/400/400   " alt="Third slide">
+                                    <img class="d-block w-100 image-responsive" src="http://lorempixel.com/400/400   " alt="Third slide">
                                 </div>
                             </div>
                             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -52,15 +52,15 @@
 
             </div>
             <div class="col-md-7">
-                <div class="card">
+                <div class="card h-100">
                     <div class="card-body">
                         <h3 class="card-title text-center card-c-element card-body">M1 GARAND</h3>
                         <div id="price" class="card-c-element card-body">
-                            <h3 class="card-title text-center" id="price">$100</h3>
+                            <h3 class="card-title text-center" id="price">€100</h3>
                         </div>
-                        <div id="bid-button" class="text-center card-body">
+                        <div  class="text-center card-body">
                             <input type="number" class="add-margin text-center" value="110"></input>
-                            <button type="button" class="btn btn-block btn-primary btn-lg center-block">Bid Now</button>
+                            <button type="button" class="btn btn-block btn-primary btn-lg center-block" id="bid-button">Bid Now</button>
                         </div>
                     </div>
                 </div>
@@ -70,15 +70,15 @@
 
         <div class="card">
             <div class="card-body">
-                <h3 class="card-title">Documentation</h3>
+                <h3 class="card-title text-center">Documentation</h3>
                 <p class="card-text"></p>
             </div>
         </div>
 
         <div class="card">
             <div class="card-body">
-                <h3 class="card-title">Price variation</h3>
-                <img src="../assets/chart.png">
+                <h3 class="card-title text-center">Price variation</h3>
+                <img src="../assets/chart.png" class="img-responsive" width="100%"/>
             </div>
 
         </div>
