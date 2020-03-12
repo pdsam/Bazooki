05b7db33060a33221047cae0204c1ee7dbbc7a53
JosA(c) Nuno Branco Lima Oliveira e Silva <up201705591@gnomo.fe.up.pt>
2020-03-12 13:58:02 +0000

    'Create Auction' page done

--- pages/create_auction.php
+++ pages/create_auction.php
@@ -11,81 +11,41 @@
     <link rel="stylesheet" href="<?= $files_path ?>/css/components/footer.css">
 </head>
 
-
-
 <body class="bg-light">
     <div class="container mt-2">
         <?php include_once(getcwd() . '/../templates/navbar.php') ?>
-        <div class="row" id="product-details">
-            <div class="col-md-5">
-                <div class="card h-100">
-                    <div class="shadow-lg">
-                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                            </ol>
+        <div>
+			<form>
+				<div class="form-group mt-4 ml-4 mr-4">
+					<input type="email" class="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Title">
+				</div>
+				
+				<div class="form-group mt-4 ml-4 mr-4">
+					<textarea class="form-control" rows="5" id="description" placeholder="Description"></textarea>
+				</div>
+				
+				<div class="card ml-4 mr-4">
+					<div class="card-body">
+						<div class="form-check">
+							<input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+							<label class="form-check-label" for="defaultCheck1">
+							I confirm I have a certificate and it's genuine.
+							</label>
+						</div>
+						<input type="file" class="form-control-file" id="exampleFormControlFile1">
+					</div>
+				</div>
 
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img class="d-block w-100 image-responsive" src="http://lorempixel.com/400/400" alt="First slide">
-                                </div>
-                                <div class="carousel-item">
-                                    <img class="d-block w-100 image-responsive" src="http://lorempixel.com/400/400" alt="Second slide">
-                                </div>
-                                <div class="carousel-item">
-                                    <img class="d-block w-100 image-responsive" src="http://lorempixel.com/400/400   " alt="Third slide">
-                                </div>
-                            </div>
-                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+				<div class="form-group mt-4 ml-4 mr-4">
+					<input type="email" class="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Title">
+				</div>
 
-            </div>
-            <div class="col-md-7">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <h3 class="card-title text-center card-c-element card-body">M1 GARAND</h3>
-                        <div id="price" class="card-c-element card-body">
-                            <h3 class="card-title text-center" id="price">€100</h3>
-                        </div>
-                        <div  class="text-center card-body">
-                            <input type="number" class="add-margin text-center" value="110"></input>
-                            <button type="button" class="btn btn-block btn-primary btn-lg center-block" id="bid-button">Bid Now</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+			<div>
+			<button type="submit" class="btn btn-primary ml-4">Submit</button>
+			</div>
+			</form>
         </div>
-
-        <div class="card">
-            <div class="card-body">
-                <h3 class="card-title text-center">Documentation</h3>
-                <p class="card-text"></p>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="card-body">
-                <h3 class="card-title text-center">Price variation</h3>
-                <img src="../assets/chart.png" class="img-responsive" width="100%"/>
-            </div>
-
-        </div>
-
 
         <?php include_once('../templates/footer.php'); ?>
     </div>
-</body>
-
 </body>
