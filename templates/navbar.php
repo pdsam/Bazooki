c4624c939c81b7d40760573964ce1f16d8afcbeb
Moisés Rocha <moisesrocha99@gmail.com>
2020-03-12 10:05:19 +0000

    removed faq and about links, added search icon

--- templates/navbar.php
+++ templates/navbar.php
@@ -12,27 +12,22 @@
             <div class="input-group">
                 <input class="w-auto form-control rounded-0" type="search" placeholder="Search" aria-label="Search">
                 <button class="input-group-light btn btn-reverse-purple rounded-0" type="submit">
+                    <i class="fa fa-search p-0"></i>
                 Search
                 </button>
             </div>
         </form>
         <ul class="navbar-nav">
-        <li class="nav-item active ">
-            <a class="nav-link custom-hover" id="button1" href="<?= $files_path ?>/pages/faq.php">FAQ<span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item active ">
-            <a class="nav-link custom-hover" id="button2" href="#">About</a>
-        </li>
         <li class="nav-item active dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fa fa-user p-0"></i>
-            User
+                <i class="fa fa-user p-0"></i>
+                User
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="<?= $files_path ?>/pages/profile.php">Profile</a>
-            <a class="dropdown-item" href="#">Account Settings</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="<?= $files_path ?>/pages/login.php">Log out</a>
+                <a class="dropdown-item" href="<?= $files_path ?>/pages/profile.php">Profile</a>
+                <a class="dropdown-item" href="#">Account Settings</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="<?= $files_path ?>/pages/login.php">Log out</a>
             </div>
         </li>
         </ul>
