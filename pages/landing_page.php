2c7f24db1211015a2b8e922aa45656753eb4725e
GambuzX <mg_mesquita@hotmail.com>
2020-03-12 16:19:15 +0000

    stuff

--- pages/landing_page.php
+++ pages/landing_page.php
@@ -62,11 +62,11 @@
             </div>
         </div>
         <div class="row carousel-controls">
-            <a class="col-1 btn btn-olive btn-lg" href="#<?=$id?>" role="button" data-slide="prev">
+            <a class="col-lg-1 col-sm-6 btn btn-olive btn-lg" href="#<?=$id?>" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
             </a>
-            <a class="col-1 btn btn-olive btn-lg" href="#<?=$id?>" role="button" data-slide="next">
+            <a class="col-lg-1 col-sm-6 btn btn-olive btn-lg" href="#<?=$id?>" role="button" data-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
