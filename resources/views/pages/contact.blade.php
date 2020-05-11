b9540fb47f6ac392672e76068aabefa8bcd72827
GambuzX <mg_mesquita@hotmail.com>
2020-05-11 21:45:20 +0100

    Terms and conditions

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Bazooki - Contact Us')
+
+@section('head')
+@endsection
+
+@section('content')
+<div class="jumbotron section-jumbotron">
+    <h1 class="display-4">Contact Us <i class="fas fa-envelope"></i></h1>
+    <hr class="my-2">
+    <p class="lead">“One good conversation can shift the direction of change forever.”</p>
+    <p>- Linda Lambert</p>
+</div>
+@endsection
---
+++ resources/views/pages/terms.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('title', 'Bazooki - Terms and Conditions')
+
+@section('head')
+    <!--<link rel="stylesheet" href={{ asset('css/contact_us.css') }}>-->
+@endsection
+
+@section('content')
+<div class="jumbotron section-jumbotron">
+    <h1 class="display-4">Terms and Conditions <i class="fas fa-file-contract"></i></h1>
+    <hr class="my-2">
+    <p class="lead">“The devil is compromise.”</p>
+    <p>- Henrik Ibsen</p>
+</div>
+<div>
+    <p>Last updated: 11/05/2020</p>
+    <p>Please read these Terms and Conditions ("Terms", "Terms and Conditions") carefully before using
+    the Bazooki website.</p>
+    <p>Your access to and use of the Service is conditioned on your acceptance of and compliance with
+    these Terms. These Terms apply to all visitors, users and others who access or use the Service.</p>
+    <p><strong>By accessing or using the Service you agree to be bound by these Terms. If you disagree
+    with any part of the terms then you may not access the Service.</strong></p>
+    <h5>Purchases</h5>
+    <p>If you wish to purchase any product or service made available through the Service ("Purchase"),
+    you may be asked to supply certain information relevant to your Purchase.</p>
+    <h5>Content</h5>
+    <p>Our Service allows you to post, link, store, share and otherwise make available certain information,
+    text, graphics, videos, or other material ("Content"). You are responsible for your usage.</p>
+    <h5>Links To Other Web Sites</h5>
+    <p>Our Service may contain links to third-party web sites or services that are not owned or controlled
+    by Bazooki.</p>
+    <p>Bazooki has no control over, and assumes no responsibility for, the content,
+    privacy policies, or practices of any third party web sites or services. You further acknowledge and
+    agree that Bazooki shall not be responsible or liable, directly or indirectly, for any
+    damage or loss caused or alleged to be caused by or in connection with use of or reliance on any
+    such content, goods or services available on or through any such web sites or services.</p>
+    <h5>Changes</h5>
+    <p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a
+    revision is material we will try to provide at least 30 (change this) days' notice prior to any new terms
+    taking effect. What constitutes a material change will be determined at our sole discretion.</p>
+    <h5>Contact Us</h5>
+    <p>If you have any questions about these Terms, please contact us.</p>
+</div>
+@endsection
