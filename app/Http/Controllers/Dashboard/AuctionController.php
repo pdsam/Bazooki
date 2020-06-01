017534035cb4cbfea47fbe7554d7ced0679e7342
pdsam <paulodsam@gmail.com>
2020-06-01 15:42:49 +0100

    restoring mod checks

--- app/Http/Controllers/Dashboard/AuctionController.php
+++ app/Http/Controllers/Dashboard/AuctionController.php
@@ -11,11 +11,11 @@ class AuctionController extends Controller
 {
     public function show()
     {
-        /*
+        
         if(!Auth::guard('mod')->check() && !Auth::guard('admin')->check()) {
             return Redirect::back()->withErrors(['You do not have permission to access that resource.', '┬┴┬┴┤ ͜ʖ ͡°) ├┬┴┬┴']);
         }
-        */
+        
         $auctions = Auction::whereRaw('start_time + duration * interval \'1 second\' > CURRENT_TIMESTAMP');
         $auctions = $auctions->get();
         return view('dashboard.auctions',['auctions' =>$auctions]);
