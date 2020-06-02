6f072c6c6f88802b1efdf73c92a3b448f162b231
pdsam <paulodsam@gmail.com>
2020-06-02 15:09:58 +0100

    work on controller

--- app/Http/Controllers/Dashboard/UserController.php
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Bazooker;
+use App\Ban;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Exception;
 
 class UserController extends Controller
 {
@@ -25,12 +27,34 @@ class UserController extends Controller
             return Redirect::back()->withErrors(['You do not have permission to access that resource.', '┬┴┬┴┤ ͜ʖ ͡°) ├┬┴┬┴']);
         }
 
+        if(Auth::guard('mod')->check()){
+            $modID = Auth::guard('mod')->user()->id;
+
+        }
+        if(Auth::guard('admin')->check()){
+            $modID = Auth::guard('admin')->user()->mod->id;
+
+        }
+
         return Redirect::back()->withErrors(["WIP SUSPEND"]);
     }
 
     public function ban($id){
-        if(!Auth::guard('mod')->check() && !Auth::guard('admin')->check()) {
+        if(!Auth::guard('admin')->check()) {
             return Redirect::back()->withErrors(['You do not have permission to access that resource.', '┬┴┬┴┤ ͜ʖ ͡°) ├┬┴┬┴']);
+        }
+
+        try{
+       $ban =  Ban::create([
+            'reason' => 'Please email us for that',
+            'admin_id' => Auth::guard('admin')->user()->mod->id,
+            'bazooker_id' => $id
+
+
+       ]);
+        }
+        catch(Exception $e){
+            return Redirect::back()->withErrors(["User already banend"]);
         }
 
 
